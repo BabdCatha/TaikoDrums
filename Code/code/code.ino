ae8b5ec39d8520bae2ef80873d5eec589254454d
BabdCatha <36963178+BabdCatha@users.noreply.github.com>
2022-10-06 19:53:58 +0200

    Add BLE Keyboard function

--- Code/code/code.ino
+++ Code/code/code.ino
@@ -1,14 +1,33 @@
+//BLE HID device library from https://github.com/T-vK/ESP32-BLE-Keyboard
+#include <BleKeyboard.h>
+
+#define PIN_1 13
+#define PIN_2 13
+#define PIN_3 14
+#define PIN_4 27
+
+#define KEY_1 "D"
+#define KEY_2 "F"
+#define KEY_3 "J"
+#define KEY_4 "K"
+
+//Creating the BLE Keyboard
+BleKeyboard bleKeyboard("Taiko Drum");
 
 void setup() {
   Serial.begin(115200);
   delay(1000); // give me time to bring up serial monitor
   Serial.println("ESP32 Touch Test");
+
+  //Launching the BLE Keyboard
+  bleKeyboard.begin();
+  
 }
 
 void loop() {
-  Serial.println(touchRead(13));
-  Serial.println(touchRead(12));
-  Serial.println(touchRead(14));
-  Serial.println(touchRead(27));
-  delay(1000);
+  Serial.println(touchRead(PIN_4));
+  if(touchRead(PIN_4) < 20){
+    bleKeyboard.print(KEY_4);
+  }
+  delay(1);
 }
