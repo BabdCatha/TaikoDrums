4c62be955664d6d485a3a6eb7277cadf09f4b455
BabdCatha <36963178+BabdCatha@users.noreply.github.com>
2022-10-16 18:50:09 +0200

    Switch to an Arduino Pro micro
    This allows us to make the drum appear as a simple keyboard to the OS, simplifying the setup process.
    Also switched to piezo sensors for detection.

--- Code/code/code.ino
+++ Code/code/code.ino
@@ -1,33 +1,42 @@
-//BLE HID device library from https://github.com/T-vK/ESP32-BLE-Keyboard
-#include <BleKeyboard.h>
+#include "Keyboard.h"
 
-#define PIN_1 13
-#define PIN_2 13
+#define PIN_1 A0
+#define PIN_2 A2
 #define PIN_3 14
 #define PIN_4 27
 
-#define KEY_1 "D"
-#define KEY_2 "F"
+#define KEY_1 68
+#define KEY_2 70
 #define KEY_3 "J"
 #define KEY_4 "K"
 
-//Creating the BLE Keyboard
-BleKeyboard bleKeyboard("Taiko Drum");
+int lastPressTime[4] = {0, 0, 0, 0};
 
 void setup() {
-  Serial.begin(115200);
-  delay(1000); // give me time to bring up serial monitor
-  Serial.println("ESP32 Touch Test");
 
+  Serial.begin(115200);
   //Launching the BLE Keyboard
-  bleKeyboard.begin();
+  Keyboard.begin();
   
 }
 
 void loop() {
-  Serial.println(touchRead(PIN_4));
-  if(touchRead(PIN_4) < 20){
-    bleKeyboard.print(KEY_4);
+  Serial.println(analogRead(PIN_1));
+  if(analogRead(PIN_1) > 200 && lastPressTime[0] == 0){
+    Keyboard.press(KEY_1);
+    delay(1);
+    lastPressTime[0] = 120;
+    Keyboard.release(KEY_1);
+  }else if(analogRead(PIN_1) < 200 && lastPressTime[0]){
+    lastPressTime[0]--;
   }
-  delay(1);
+
+  if(analogRead(PIN_2) > 200 && lastPressTime[1] == 0){
+    Keyboard.press(KEY_2);
+    delay(1);
+    lastPressTime[1] = 120;
+    Keyboard.release(KEY_2);
+  }else if(analogRead(PIN_2) < 200 && lastPressTime[1]){
+    lastPressTime[1]--;
+  }
 }
